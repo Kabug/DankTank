53fa03cff9183625f0301af98bbba457d2beea39
Kabug <Kabilanm.thangarajah@gmail.com>
2021-09-11 22:18:05 -0700

    Added Camera display and a couple ordinal directions

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
+from picamera import PiCamera
 from pynput import keyboard
-from gpiozero import Robot
 import RPi.GPIO as GPIO
 import time
 import os
 
+# Initial Values
+ 
 GPIO.setmode(GPIO.BCM)
 left_motor = [22, 23]
 right_motor = [17, 27]
@@ -11,15 +13,22 @@ GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
+
+camera = PiCamera()
+camera.rotation = 180
+
 speed = 1
+correction = 0.0003
 run = True
-use_camera = False
 controls_state = {
-    "w": False,
-    "s": False,
-    "a": False,
-    "d": False
+    "w": False, # Forward
+    "a": False, # Left
+    "s": False, # Backward
+    "d": False, # Right
+    "c": False  # Camera
 }
+
+# Keyboard Listener
 
 def on_press(key):
     try:
@@ -33,6 +42,8 @@ def on_press(key):
                 controls_state["s"] = True
             elif key.char == ("d"):
                 controls_state["d"] = True
+            elif key.char == ("c"):
+                controls_state["c"] = not(controls_state["c"])
         else:
             stop()
     except AttributeError:
@@ -63,14 +74,16 @@ kblistener = keyboard.Listener(
         on_release=on_release)
 kblistener.start() 
 
+# Movement Functions
+
 def go_forwards():
     GPIO.output(left_motor[0], GPIO.HIGH)
     GPIO.output(left_motor[1], GPIO.LOW)
     GPIO.output(right_motor[0], GPIO.HIGH)
     GPIO.output(right_motor[1], GPIO.LOW)
-    time.sleep(0.0003)
+    time.sleep(correction)
     GPIO.output(left_motor[0], GPIO.LOW)
-    time.sleep(0.0003)
+    time.sleep(correction)
     GPIO.output(left_motor[0], GPIO.HIGH)
 
 def go_left():
@@ -84,9 +97,9 @@ def go_backwards():
     GPIO.output(left_motor[1], GPIO.HIGH)
     GPIO.output(right_motor[0], GPIO.LOW)
     GPIO.output(right_motor[1], GPIO.HIGH)
-    time.sleep(0.0003)
+    time.sleep(correction)
     GPIO.output(left_motor[0], GPIO.HIGH)
-    time.sleep(0.0003)
+    time.sleep(correction)
     GPIO.output(left_motor[0], GPIO.LOW)
 
 def go_right():
@@ -95,22 +108,54 @@ def go_right():
     GPIO.output(right_motor[0], GPIO.LOW)
     GPIO.output(right_motor[1], GPIO.HIGH)
 
+def go_forward_right():
+    GPIO.output(left_motor[0], GPIO.HIGH)
+    GPIO.output(left_motor[1], GPIO.LOW)
+    GPIO.output(right_motor[0], GPIO.HIGH)
+    GPIO.output(right_motor[1], GPIO.LOW)
+    time.sleep(correction * 5)
+    GPIO.output(right_motor[0], GPIO.LOW)
+    time.sleep(correction * 5)
+    GPIO.output(right_motor[0], GPIO.HIGH)
+
+def go_forward_left():
+    GPIO.output(left_motor[0], GPIO.HIGH)
+    GPIO.output(left_motor[1], GPIO.LOW)
+    GPIO.output(right_motor[0], GPIO.HIGH)
+    GPIO.output(right_motor[1], GPIO.LOW)
+    time.sleep(correction * 500)
+    GPIO.output(left_motor[0], GPIO.LOW)
+    time.sleep(correction * 500)
+    GPIO.output(left_motor[0], GPIO.HIGH)
+
 def stop():
     GPIO.output(left_motor[0], GPIO.LOW)
     GPIO.output(left_motor[1], GPIO.LOW)
     GPIO.output(right_motor[0], GPIO.LOW)
     GPIO.output(right_motor[1], GPIO.LOW)
 
+# Main Loop
+
 while run == True:
     if controls_state["w"]:
-        go_forwards()
+        if controls_state["d"]:
+            go_forward_right()
+        elif controls_state["a"]:
+            go_forward_left()
+        elif controls_state["s"]:
+            stop()
+        else:
+            go_forwards()
     elif controls_state["a"]:
         go_left()
     elif controls_state["s"]:
         go_backwards()
     elif controls_state["d"]:
         go_right()
-
+    
+    if controls_state["c"]:
+        camera.start_preview()
+    
     onTime = 0.001
     if speed == 0:
         onTime = 0
@@ -121,5 +166,8 @@ while run == True:
     stop()
     time.sleep(offTime)
 
+# Cleanup
+
 kblistener.stop()
+camera.close()
 GPIO.cleanup()
